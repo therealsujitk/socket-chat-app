7f468e00c92552d63821e1595ee800843b09b132
Sujit Kumar <kumarsujit2002@gmail.com>
2020-07-26 21:32:55 +0530

    Added basic socket.io functionality

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello World!</h1>');
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/'));
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	
+	socket.on('disconnect', () => {
+		console.log('a user disconnected');
+	});
+	
+	socket.on('message', (message) => {
+		io.emit('message', message);
+	});
 });
 
 http.listen(3000, () => {
