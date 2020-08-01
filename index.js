0d3075cd3fde0f27d11067f65af4f398169b9dcc
Sujit Kumar <kumarsujit2002@gmail.com>
2020-08-01 21:22:31 +0530

    Added function to load all the online users

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var users = [];
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
@@ -12,13 +14,22 @@ app.use(express.static(__dirname + '/'));
 io.on('connection', (socket) => {
 	console.log('a user connected');
 	
+	socket.emit('online users', users);
+
 	socket.on('disconnect', () => {
 		console.log('a user disconnected');
 	});
-	
+
 	socket.on('message', (message) => {
 		if(message.trim() != '')
 			socket.broadcast.emit('message', message);
+	});
+	
+	socket.on('new user', (user) => {
+		if(user.trim() != '') {
+			socket.broadcast.emit('new user', user);
+			users.push(user);
+		}
 	});
 });
 
