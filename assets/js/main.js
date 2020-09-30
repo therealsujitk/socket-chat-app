9480a162227396339a5b8945b3c0aa212153fb6e
Sujit Kumar <kumarsujit2002@gmail.com>
2020-09-30 19:26:56 +0530

    Minor improvements and bug fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+var socket = io();
+var selectedUser = 'everyone';
+var savedUsername;
+var calcMessages = 0;
+var i = 0;
+
 function openUsername() {
 	let x = document.getElementById('username-modal');
 	let y = document.getElementById('modal-background');
@@ -85,4 +91,198 @@ function switchInterface(id) {
 	}
 	
 	messages.style.display = "block";
+
+	document.getElementById('chat-box').focus();
 }
+
+function setIndex() {
+	let headerHeight = document.getElementById('header').offsetHeight;
+	let chatInterfaceHeaderHeight = document.getElementById('chat-header').offsetHeight;
+
+	let chatInterfaceHeight = window.innerHeight - headerHeight - chatInterfaceHeaderHeight - 25 - 25 - 6;
+	document.documentElement.style.setProperty('--cih', `${chatInterfaceHeight}px`);
+
+	let chatFooterHeight = document.getElementById('chat-footer').offsetHeight;
+
+	let messagesHeight = chatInterfaceHeight - chatFooterHeight - 4;
+	document.documentElement.style.setProperty('--mh', `${messagesHeight}px`);
+	calcMessages = Math.round(messagesHeight);
+
+	window.addEventListener("resize", setIndex);
+
+	setTimeout(function() {
+		document.getElementById('username').focus();
+	}, 500);
+}
+
+function toggleMenu (){
+	let x = document.getElementById('menu');
+	let y = document.getElementById('nav');
+	let z = document.getElementById('menu-background');
+
+	if(i%2 == 0) {
+		x.classList.add('rotate-menu');
+		y.classList.add('show-nav');
+		z.style.display = "block";
+	}
+	else {
+		x.classList.add('rotate-menu-again');
+		y.classList.add('hide-nav');
+
+		setTimeout(function() {
+			x.classList.remove('rotate-menu');
+			x.classList.remove('rotate-menu-again');
+			y.classList.remove('show-nav');
+			y.classList.remove('hide-nav');
+			z.style.display = "none";
+		}, 500);
+	}
+
+	++i;
+}
+
+function sendMessage() {
+	if(typeof savedUsername === "undefined") {
+		openUsername();
+		return;
+	}
+
+	var message = document.getElementById('chat-box');
+	
+	if(selectedUser === "everyone") {
+		let temp = {};
+		temp[savedUsername] = message.value.trim();
+		socket.emit('broadcast', temp);
+		
+		if(message.value.trim() != '') {
+			let flag = false;
+			let messages = document.getElementById('messages-everyone');
+			if(messages.scrollTop === messages.scrollHeight-calcMessages)
+				flag = true;
+			messages.innerHTML = messages.innerHTML + '<div style="width: 100%; text-align: right;"><div class="message" style="background: #fff; color: #000;">' + message.value.trim() + '</div></div>';
+			if(flag)
+				messages.scrollTop = messages.scrollHeight;
+		}
+	}
+	else {
+		let temp = {};
+		temp[selectedUser] = message.value.trim();
+		socket.emit('private', temp);
+		
+		if(message.value.trim() != '') {
+			let flag = false;
+			let messages = 'messages-' + selectedUser;
+			messages = document.getElementById(messages);
+			if(messages.scrollTop === messages.scrollHeight-calcMessages)
+				flag = true;
+			messages.innerHTML = messages.innerHTML + '<div style="width: 100%; text-align: right;"><div class="message" style="background: #fff; color: #000;">' + message.value.trim() + '</div></div>';
+			if(flag)
+				messages.scrollTop = messages.scrollHeight;
+		}
+	}
+
+	message.value = '';
+	document.getElementById('chat-box').focus();
+}
+
+function enterSend(e) {
+	if(e.keyCode == 13)
+		sendMessage();
+}
+
+socket.on('private', (data) => {
+	let id = Object.keys(data)[0];
+	let user = 'user-' + id;
+	let flag = false;
+	user = document.getElementById(user);
+	let messages = 'messages-' + id;
+	messages = document.getElementById(messages);
+	
+	if(messages.scrollTop === messages.scrollHeight-calcMessages)
+		flag = true;
+	
+	let message = data[id];
+	
+	messages.innerHTML += '<div style="width: 100%; text-align: left;"><div class="message" style="background: #000; color: #fff;">' + message + '</div></div>';
+	if(flag)
+		messages.scrollTop = messages.scrollHeight;
+	if(selectedUser != id)
+		user.style.color = "#fff";
+});
+
+socket.on('broadcast', (data) => {
+	let flag = false;
+	let user = Object.keys(data)[0];
+	let messages = document.getElementById('messages-everyone');
+	let message = data[Object.keys(data)[0]];
+	if(messages.scrollTop === messages.scrollHeight-calcMessages)
+		flag = true;
+	messages.innerHTML += '<div style="width: 100%; text-align: left;"><div class="message" style="background: #000; color: #fff;"><b>' + user + '</b>: ' + message + '</div></div>';
+	if(flag)
+		messages.scrollTop = messages.scrollHeight;
+	
+	if(selectedUser != "everyone")
+		document.getElementById('user-everyone').style.color = "#fff";
+});
+
+function newUser() {
+	let username = document.getElementById('username');
+	
+	if(username.value.trim() == '')
+		return;
+	else {
+		socket.emit('new user', username.value.trim());
+		closeAll();
+		savedUsername = username.value.trim();
+	}
+
+	document.getElementById('chat-box').focus();
+}
+
+function enterUser(e) {
+	if(e.keyCode == 13)
+		newUser();
+}
+
+socket.on('new user', (user) => {
+	let users = document.getElementById('user-list');
+	
+	let temp = 'user-' + Object.keys(user)[0];
+	if(document.getElementById(temp)) {
+		document.getElementById(temp).innerHTML = '@' + user[Object.keys(user)[0]];
+		if(selectedUser == Object.keys(user)[0])
+			document.getElementById('chat-header').innerHTML = '@' + user[Object.keys(user)[0]];
+	}
+	else {
+		users.innerHTML += '<div id="user-' + Object.keys(user)[0] + '" class="user" style="background: none; color: #808080;" onclick="switchInterface(this.id); selectedUser = this.id.slice(5);">@' + user[Object.keys(user)[0]] + '</div>';
+		let chat = document.getElementById('chat');
+		chat.innerHTML = '<div id="messages-' + Object.keys(user)[0] + '" class="messages" style="display: none;"></div>' + chat.innerHTML;
+	}
+});
+
+socket.on('online users', (onlineUsers) => {
+	let i = 0;
+	while(i < Object.keys(onlineUsers).length) {
+		let users = document.getElementById('user-list');
+		users.innerHTML += '<div id="user-' + Object.keys(onlineUsers)[i] + '" class="user" style="background: none; color: #808080;" onclick="switchInterface(this.id); selectedUser = this.id.slice(5);">@' + onlineUsers[Object.keys(onlineUsers)[i]] + '</div>';
+		let chat = document.getElementById('chat');
+		chat.innerHTML = '<div id="messages-' + Object.keys(onlineUsers)[i] + '" class="messages" style="display: none;"></div>' + chat.innerHTML;
+		++i;
+	}
+});
+
+socket.on('delete user', (id) => {
+	let user = 'user-' + id;
+	user = document.getElementById(user);
+	let messages = 'messages-' + id;
+	messages = document.getElementById(messages);
+	
+	if(selectedUser == id) {
+		messages.style.display = "none";
+		document.getElementById('messages-everyone').style.display = "block";
+		selectedUser = "everyone";
+	}
+	
+	user.remove();
+	messages.remove();
+});
